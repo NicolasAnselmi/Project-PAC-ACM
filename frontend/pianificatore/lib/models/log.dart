505c3e67c71a7857f99e4e850bc0247af8094d83
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2023-01-16 17:12:31 +0100

    fixed log parser

--- frontend/pianificatore/lib/models/log.dart
+++ frontend/pianificatore/lib/models/log.dart
@@ -1,9 +1,13 @@
+import 'package:pianificatore/utils/enums.dart';
+
 class Log {
-  final int idLog;
+  final String idLog;
   final String idLogger;
   final String title;
   final String body;
   final String timeStamp;
+  final StatoMacchina statoMacchina;
+  final String codiceLotto;
 
   Log({
     required this.idLogger,
@@ -11,6 +15,8 @@ class Log {
     required this.title,
     required this.body,
     required this.timeStamp,
+    required this.statoMacchina,
+    required this.codiceLotto,
   });
 
   factory Log.fromJson(Map<String, dynamic> json) {
@@ -20,6 +26,8 @@ class Log {
       title: json["title"],
       body: json["body"],
       timeStamp: json["timeStamp"],
+      statoMacchina: StatoMacchina.values.byName(json["statoMacchina"]),
+      codiceLotto: json["codiceLotto"],
     );
   }
 }
--- frontend/pianificatore/lib/notifiers/user_auth_notifier.dart
+++ frontend/pianificatore/lib/notifiers/user_auth_notifier.dart
@@ -9,7 +9,7 @@ class UserAuthNotifier extends Notifier<UserStatus> {
 
   void login(String email, String password) {
     // TODO: RIMUOVEREa
-    state = UserStatus.pianificatore;
+    state = UserStatus.operaio;
     return;
 
     email = email.trim();
